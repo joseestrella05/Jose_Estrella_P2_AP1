99babe4d2c60ac473c883da1e84b094e11c27d1a
Jose Gabriel <Jose Gabriel@DESKTOP-SQIUKTF>
2025-03-10 18:43:37 -0400

    Add encuesta class

---
+++ Models/Encuestas.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Jose_Estrella_P2_AP1.Models;
+
+public class Encuestas
+{
+    [Key]
+    public int EncuestasId { get; set; }  
+
+    [Required(ErrorMessage = "La fecha es obligatoria")]
+    public DateTime Fecha {  get; set; }
+
+    [Required(ErrorMessage = "La asignatura es obligatoria")]
+    public string? Asignatura { get; set; }
+}
